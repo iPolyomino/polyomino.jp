0cd8bdd9c222cd79694cfe80ceaa5b309dea5455
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-07-31 12:49:24 +0900

    add locale

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ["ja"],
+    defaultLocale: "ja",
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
