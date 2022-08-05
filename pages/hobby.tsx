0ab418ca61c947b861f1b68923ab4661829adb83
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-08-05 20:17:05 +0900

    add hobby page

---
+++ pages/hobby.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Grid from "@mui/material/Grid";
+
+import NavigationBar from "../components/NavigationBar";
+import CenterrizedHorizontalGrid from "../components/CenterrizedHorizontalGrid";
+import Footer from "../components/Footer";
+
+const Hobby: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Hagi - Blog</title>
+        <meta name="description" content="Hagi's portfolio website." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavigationBar />
+      <CenterrizedHorizontalGrid>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={12}>
+          <Grid item xs={12}>
+            <Footer />
+          </Grid>
+        </Grid>
+      </CenterrizedHorizontalGrid>
+    </>
+  );
+};
+
+export default Hobby;
