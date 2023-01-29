8f07992326638a4ccfcae51aef04d4a27a303214
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2023-01-30 07:14:56 +0900

    update packages

--- next.config.js
+++ next.config.js
@@ -1,22 +1,19 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+});
 
-const settings = withPWA({
+module.exports = withPWA({
   i18n: {
     locales: ["ja"],
     defaultLocale: "ja",
   },
   reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    register: true,
-    skipWaiting: true,
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/],
-  },
 });
-
-module.exports = settings;
