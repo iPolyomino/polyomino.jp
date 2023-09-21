0e451b070841f92c9491200c34608c51c71b33b9
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2023-09-21 22:00:44 +0900

    add rhythmorse support page

---
+++ components/Rhythmorse.tsx
@@ -0,0 +1,12 @@
+import ContentsCard from "@/components/ContentsCard";
+
+const Rythmorse = () => {
+  return (
+    <ContentsCard>
+      <h1>Rythmorse Privacy Policy</h1>
+      <p>This application does not collect user information.</p>
+    </ContentsCard>
+  );
+};
+
+export default Rythmorse;
---
+++ pages/support/rhythmorse.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Grid from "@mui/material/Grid";
+
+import NavigationBar from "@/components/NavigationBar";
+import CenterrizedHorizontalGrid from "@/components/CenterrizedHorizontalGrid";
+import Rhythmorse from "@/components/Rhythmorse";
+import Footer from "@/components/Footer";
+
+const RhythmorsePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Rhythmorse support</title>
+        <meta name="" content="Rhythmorse support" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavigationBar />
+      <CenterrizedHorizontalGrid>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={12}>
+          <Grid item xs={12}>
+            <Rhythmorse />
+          </Grid>
+          <Grid item xs={12}>
+            <Footer />
+          </Grid>
+        </Grid>
+      </CenterrizedHorizontalGrid>
+    </>
+  );
+};
+
+export default RhythmorsePage;
