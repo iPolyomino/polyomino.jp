41a384de96a2d39a3beb4121ead13de9ff13ce3b
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2026-07-31 11:35:38 +0900

    update

--- components/LsnlParticles.tsx
+++ components/LsnlParticles.tsx
@@ -1,19 +1,17 @@
-import { useEffect, useState } from "react";
 import type { Engine, ISourceOptions } from "@tsparticles/engine";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
+import Particles, { ParticlesProvider } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
 
 import options from "@/contents/particles.json";
 
+const initializeParticles = async (engine: Engine) => {
+  await loadSlim(engine);
+};
+
 export default function LsnlParticles() {
-  const [initialized, setInitialized] = useState(false);
-
-  useEffect(() => {
-    initParticlesEngine(async (engine: Engine) => {
-      await loadSlim(engine);
-    }).then(() => setInitialized(true));
-  }, []);
-
-  if (!initialized) return null;
-  return <Particles id="lsnl-particles" options={options as ISourceOptions} />;
+  return (
+    <ParticlesProvider init={initializeParticles}>
+      <Particles id="lsnl-particles" options={options as ISourceOptions} />
+    </ParticlesProvider>
+  );
 }
